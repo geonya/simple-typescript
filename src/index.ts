92fc433066b9e489af761d6d6d8b2d105935729e
geony <geony@boraui-MacBookAir.local>
2023-01-31 21:03:18 +0900

    @types/node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("hi");
+const numArray: number[] = [1, 2, 3];
+
+for (let value of numArray) console.log(value);
+
+const strArray: string[] = ["heelo", "world", "!"];
+
+for (let value of strArray) console.log(value);
